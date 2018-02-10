47257c0ff91575f8454e0efc5502ececdc2454cb
MatthewShew <shewmj@gmail.com>
2018-02-10 14:41:47 -0800

    catalogue view added

---
+++ application/views/Catalogue.php
@@ -0,0 +1,18 @@
+<table>
+<tbody>
+  <div id="all-items" class="items">
+    {accessories}
+        <tr class="item_row">
+          <td><img class="draggable drag-drop " src="{ImageLocation}"/></td>
+          <td><h4>{Name}</h4></td>
+          <td><p>&nbsp;Calories: {Calories}</p></td>
+          <td><p>&nbsp;&nbsp;Salt: {Salt}</p></td>
+          <td><p>&nbsp;&nbsp;Price: {Price}</p></td>
+        </tr>
+      </div>
+    {/accessories}
+  </div>
+</tbody>
+</table>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
