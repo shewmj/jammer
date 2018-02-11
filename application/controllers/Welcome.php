52b27f76ac21468d782e632bca307a597ecc5947
JC <johncedricktee@gmail.com>
2018-02-10 18:46:00 -0800

    fixed homepage set

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -35,22 +35,6 @@ class Welcome extends Application
 		$this->render();
 	}
 
-	public function set($key)
-   {
-	   // get an array of all sets, to user their id and name
-	   $this->data['sets'] = $this->set->all();
-	   $this->data['pagetitle'] = 'Home Page';
-
-	   // for set viewer get set matching key
-	   $set = $this->set->get($key);
-	   // get image path for every accessory type
-	   $this->data['weapon'] = $this->accessories->get($set->weapon)->imagepath;
-	   $this->data['chestplate'] = $this->accessories->get($set->armor)->imagepath;
-	   $this->data['shoes'] = $this->accessories->get($set->boots)->imagepath;
-	   $this->data['helmet'] = $this->accessories->get($set->helmet)->imagepath;
-
-	   $this->data['pagebody'] = 'homepage';
-	   $this->render();
-   }
+	
 
 }
